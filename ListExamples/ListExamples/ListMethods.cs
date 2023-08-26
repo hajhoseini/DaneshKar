b8cc0509057f56ffd044a9092cd9ff606f10ecf2
Azam HajMohammadHosseini <hoseini.1988@gmail.com>
2023-08-26 13:53:36 -0700

    Add CSharp3 List Examples

---
+++ ListExamples/ListExamples/ListMethods.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListExamples
+{
+    public class ListMethods
+    {
+        public bool FindDuplicate(List<int> numbers)
+        {
+            bool result = false;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                for (int j = 0; j < numbers.Count; j++)
+                {
+                    if (i != j && numbers[i] == numbers[j])
+                    {
+                        result = true;
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+
+        public int GetSecondLargestNumber(List<int> numbers)
+        {
+            int largest = int.MinValue;
+            int second = int.MinValue;
+            foreach (int item in numbers)
+            {
+                if (item > largest)
+                {
+                    second = largest;
+                    largest = item;
+                }
+                else if (item > second)
+                    second = item;
+            }
+
+            return second;
+        }
+
+        public int GetSecondLargestNumber2(List<int> numbers)
+        {
+            var list = numbers;
+            list.Sort();
+            int largest = list[list.Count - 1];
+            list.Remove(largest);
+            return list[list.Count - 1];
+        }
+
+        public List<int> Inverse(List<int> numbers)
+        {
+            List<int> result = new List<int>();
+            for(int i = numbers.Count - 1; i >= 0; i--)
+            {
+                result.Add(numbers[i]);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ListExamples/ListExamples/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Data.Common;
+
+namespace ListExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int> { 5, 7, 6, 12, 23, 5, 16, 19, 6, 20 };
+
+            ListMethods listMethods = new ListMethods();
+
+            List<int> result = listMethods.Inverse(list);
+
+            string str = "";
+            foreach (int i in result)
+            {
+                str += i + ",";
+            }
+            Console.WriteLine("Inverse = {" + str + "}");
+
+            var find = listMethods.FindDuplicate(list);
+            if(find)
+                Console.WriteLine("The list has duplicate numbers!");
+
+            int secondLargestNumber = 0;
+            secondLargestNumber = listMethods.GetSecondLargestNumber(list);
+            secondLargestNumber = listMethods.GetSecondLargestNumber2(list);
+            Console.WriteLine("The Second Largest Number Is:" + secondLargestNumber);
+
+            Console.ReadKey();
+        }
+    }
+}
