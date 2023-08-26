49f7ae766035da39bd8ef4c3577ecb2dc3d7b388
Azam HajMohammadHosseini <hoseini.1988@gmail.com>
2023-08-26 13:55:48 -0700

    Add CSharp4 Array

---
+++ CSharp-Session4/Palindrome/Palindrome/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter String:");
+            string input = Console.ReadLine();
+            
+            //var resultstring = new string(input.ToCharArray().Reverse().ToArray());
+
+            bool result = IsPalindrome(input);
+            Console.WriteLine("Output : " + result);          
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            string palindrome = "";
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                palindrome += input[i];
+            }
+
+            return input == palindrome ? true : false;            
+        }
+    }
+}
---
+++ CSharp-Session4/SumOfTwoArrayElements/SumOfTwoArrayElements/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace SumOfTwoArrayElements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int[] arrayNumbers = { 28, 16, 75, 28, 1 };
+
+            string result = Compare(arrayNumbers, number);
+            if(result == "")
+                Console.WriteLine("Not Found!");
+            else
+                Console.WriteLine("Output: " + result);
+        }
+
+        static string Compare(int[] arrayNumbers, int number)
+        {
+            string result = "";
+
+            for(int i = 0; i < arrayNumbers.Length; i++)
+            {
+                for(int j= i + 1; j < arrayNumbers.Length; j++)
+                {
+                    if(arrayNumbers[i] + arrayNumbers[j] == number)
+                    {
+                        result = result + "[" + i + " , " + j + "]";
+                        break;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
