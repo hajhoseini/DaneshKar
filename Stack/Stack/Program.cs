c407098ba4992bc096b01664bbeb6524b7cbceb9
Azam HajMohammadHosseini <hoseini.1988@gmail.com>
2023-08-26 14:00:32 -0700

    Add CSharp7 Stack

---
+++ Stack/Stack/Program.cs
@@ -0,0 +1,27 @@
+﻿using Stack.StackExamples;
+
+namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyStack stack = new MyStack();
+
+            int[] nums = new int[] { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };            
+            int result = stack.CountOfUniqueItems(nums);
+
+            char[] input = new char[] { '(', ')'};
+            bool output = stack.IsValid(input);
+
+            input = new char[] { '(', ')', '[', ']', '{', '}' };
+            output = stack.IsValid(input);
+
+            input = new char[] { '(', ']' };
+            output = stack.IsValid(input);
+
+            input = new char[] { '(', '[', ']', ')', '[', ']', '{', '(', ')', '}' };
+            output = stack.IsValid(input);
+        }
+    }
+}
---
+++ Stack/Stack/StackExamples/MyStack.cs
@@ -0,0 +1,58 @@
+﻿namespace Stack.StackExamples
+{
+    public class MyStack
+    {
+        public bool IsValid(char[] array)
+        {
+            if (array.Length % 2 == 0)
+            {
+                Stack<char> stack = new Stack<char>();
+                char topElement = ' ';
+
+                foreach (char item in array)
+                {
+                    if(stack.Count > 0)
+                        topElement = stack.Peek();
+
+                    if (IsMatch(topElement, item))
+                        stack.Pop();
+                    else
+                        stack.Push(item);
+                }
+
+                return stack.Count == 0;
+            }
+
+            return false;
+        }
+
+        public bool IsMatch(char start, char end)
+        {
+            if (start == '[' && end == ']' ||
+                start == '(' && end == ')' ||
+                start == '{' && end == '}' ||
+                start == '>' && end == '<')
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public int CountOfUniqueItems(int[] array)
+        {
+            ;
+            Stack<int> stack = new Stack<int>();
+            stack.Push(array[0]);
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int temp = stack.Peek();
+                if (temp != array[i])
+                    stack.Push(array[i]);
+            }
+
+            return stack.Count;
+        }        
+    }
+}
