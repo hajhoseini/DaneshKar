7e1d4e4b1101dbe823cd63450fca27c4f4dcfb88
Azam HajMohammadHosseini <hoseini.1988@gmail.com>
2023-08-26 13:50:53 -0700

    Add CSharp2 Factorial And Fibonacci

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number:");
+            //long factorial = Factorial(Convert.ToInt32(Console.ReadLine()));
+            long factorial = RecursiveFactorial(Convert.ToInt32(Console.ReadLine()));
+            Console.WriteLine("result = " + factorial);
+        }
+
+        static long Factorial(int input)
+        {
+            int result = 1;
+
+            while(input > 1)
+            {
+                result *= input;
+                input--;
+            }
+
+            return result;
+        }
+
+        static long RecursiveFactorial(int input)
+        {
+            if (input == 0)
+                return 1;
+            else
+                return input * RecursiveFactorial(input - 1);
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace Fibonacci
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            Console.WriteLine("Enter Number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            Fibonacci(number);
+
+            Console.WriteLine("Enter Number:");
+            number = Convert.ToInt32(Console.ReadLine());
+            Print(RecursiveFibonacci(number));
+        }
+
+        static void Fibonacci(int count)
+        {
+            int first = 1;
+            int second = 0;
+            int sum = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                sum = first + second;
+                Print(sum);
+
+                first = second;
+                second = sum;
+            }
+        }
+
+        static void Print(int input)
+        {
+            Console.WriteLine(input);
+        }
+
+        static int RecursiveFibonacci(int count)
+        {
+            if (count == 1 || count == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return RecursiveFibonacci(count - 2) + RecursiveFibonacci(count - 1);
+            }
+        }        
+    }
+}
