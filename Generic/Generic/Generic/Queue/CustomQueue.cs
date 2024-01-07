a5325542d6cd246fcece8d620e176c36f4436fc8
hoseini <hoseini.1988@gmail.com>
2024-01-07 09:37:02 -0800

    Queue

---
+++ Generic/Generic/Generic/Queue/CustomQueue.cs
@@ -0,0 +1,37 @@
+﻿namespace Generic.Queue
+{
+    public class CustomQueue
+    {
+        /*
+        public void test()
+        {
+            Queue<string> queue = new Queue<string>();
+            queue.Enqueue("a");
+            queue.Dequeue();
+        }
+        */
+
+        int[] items = new int[10];
+        int head = 0;
+        int back = 0;
+
+        public void Enqueue(int item)
+        {            
+            items[back] = item;
+            back++;
+        }
+
+        public int Dequeue()
+        {
+            int value = items[head];
+            items[head] = 0;
+            head++;
+            return value;
+        }
+
+        public int Peek()
+        {
+            return items[head];
+        }
+    }
+}
---
+++ Generic/Generic/Generic/Queue/CustomQueueGeneric.cs
@@ -0,0 +1,38 @@
+﻿namespace Generic.Queue
+{
+    public class CustomQueueGeneric<T>
+    {
+        T[] items = new T[10];
+        int head = 0;
+        int back = 0;
+
+        public void Enqueue(T item)
+        {
+            if(back > items.Length)
+            {
+                Array.Resize(ref items, items.Length + 10);
+            }
+
+            items[back] = item;
+            back++;
+        }
+
+        public T Dequeue()
+        {
+            if(head > back)
+            {
+                return default(T);
+            }
+
+            T value = items[head];
+            items[head] = default(T);
+            head++;
+            return value;
+        }
+
+        public T Peek()
+        {
+            return items[head];
+        }
+    }
+}
